5243973c251575e7d0f0cea712560525f268f252
Arcerr <taurus..ali47>
2024-04-08 22:13:45 +0300

    Initial commit

---
+++ src/main/java/com/customermodule/www/CustomerModuleApplication.java
@@ -0,0 +1,13 @@
+package com.customermodule.www;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerModuleApplication.class, args);
+    }
+
+}
